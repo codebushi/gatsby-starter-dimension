abf7f870318481c674f055b5d27f7361ca460793
Cho Lai <qylai@utexas.edu>
2019-02-03 23:15:15 -0600

    Update header and add email for contact

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,8 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Qiuying Lai</h1>
+                <p>An environmental anaylyst that will eventually rule the world.</p>
             </div>
         </div>
         <nav>
--- src/components/Main.js
+++ src/components/Main.js
@@ -38,7 +38,7 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form method="post" action="mailto:qylai@utexas.edu">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
