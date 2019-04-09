2b9c9c4b2afec6beeacf6f17dc01e78ef723d714
adunis <aleksandarpetrovic101@gmail.com>
2019-04-09 22:09:08 +0200

    Added github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">Photo by Trần Anh Tuấn on Unsplash</p>
+        <p className="copyright">Photo by Trần Anh Tuấn on Unsplash  </p> 
+  <a href="https://github.com/adunis/adunis-portfolio" className="icon fa-github"><span className="label">Code </span></a>
     </footer>
 )
 
