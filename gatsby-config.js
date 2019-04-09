4be93c81f0a969db6137b8e90faab8251d37ba26
adunis <aleksandarpetrovic101@gmail.com>
2019-04-09 22:21:17 +0200

    Changed theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
-        theme_color: '#663399',
+        theme_color: '#788e63',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
