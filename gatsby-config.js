a0a3ccd50f5a67c8cdb6895ad6bb0f2b1b551c4f
adunis <aleksandarpetrovic101@gmail.com>
2019-04-18 14:07:41 +0200

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         resolve: `gatsby-plugin-google-analytics`,
         options: {
           // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-          trackingId: "UA-XXXXXXXXX-X",
+          trackingId: "UA-138587587-1",
         }
       },
       'gatsby-plugin-sass',
