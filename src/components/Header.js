87950e36f2ff540f647e22d191804f0b067f3308
sisteraram <sisteraram@naver.com>
2019-07-20 22:03:16 +0900

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
+                <h1>Aram</h1>
                 <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
                 for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
             </div>
