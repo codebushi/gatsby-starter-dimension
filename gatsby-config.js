2de9cf2e480c5f87b4968e78d77c5c230cba4bd2
Josh Seltzer <jyseltz@gmail.com>
2020-11-21 17:23:56 -0500

    update project metadata / page title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'MAPLE Beta',
+    author: 'Josh Seltzer',
+    description: 'A Gatsby.js website based on Dimension V2 by Hunter Chang',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
