f8654fe55ec00ebc7701dac9a89c0f5dd1f0beec
Josh Seltzer <jyseltz@gmail.com>
2020-11-22 10:26:02 -0500

    add meerus favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/Meerus_nobg_small.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
